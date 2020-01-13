1dd0bc8ffe007ce09ea311fc3a15d2306c0c7a44
ckoshien <ckoshien@gmail.com>
2020-01-13 12:17:50 +0900

    no message

--- config/youtubeConfig.js
+++ config/youtubeConfig.js
@@ -1,214 +1,32 @@
 export const youtubeConfig=
 {
-  //14-09-28
-  "99":"-4Dw6zQaB5I",
-  
-  //15-09-05
-  "127":"gyUMTYMOQPc",
-  //15-09-27
-  "134":"EHGM3ghFJ44",
-  //15-10-18
-  "136":"3PRR54CHsQM",
-
-  //17-06-04
-  "223":"jJBFSvwSY5k",
-  "224":"ddAeiGCFmpU",
-  "225":"XHn7-E5m21g",
-  "226":"BjFja_4cHDo",
-  //17-06-18
-  "227":"H5TwqMimTLg",
-  "228":"yUYK4a8RYO8",
-  "229":"nl-nVaeU6kw",
-  
-  //17-07-30
-  "230":"htrgyCcASOY",
-  "231":"351WFSGUZgw",
-  "232":"w7NvgTI4A-w",
-  "233":"_DjS97on2B0",
-  "234":"mhOFOke7MOI",
-
-  //17-08-27
-  "235":"i1J7_O4bfJw",
-  "236":"xjA-TUKsOKY",
-  "237":"dw8ii-T8mv0",
-  "238":"PERGkDR3UfQ",
-
-
-  //17-09-16
-  "239":"qzn5w-5pHdw",
-  
-  //17-11-04
-  "242":"jUs03p6RP1s",
-  "243":"9hCcE_9OBv4",
-  "244":"dBKy8JxY9NQ",
-
-  //17-11-12
-  "245":"twV4SAfrA58",
-  "246":"g_EP3z-WkWk",
-  "247":"hdWb7E69yZs",
-  
-  //17-11-25
-  "248":"1vBgiQhGW3E",
-  "249":"hnF0JpGWzrk",
-  "250":"QucROoxDudg",
-  
-  //17-11-26
-  "251":"oXsYByuwObc",
-  "252":"Jnw7_lzqY4g",
-  "253":"jflNBQ5FBP4",
-  "255":"zYtp6OvJXhE",
-
-  //17-12-23
-  "256":"9yjcjVns2KE",
-  "257":"PHttHFZbRMY",
-  "258":"mEt3VhaWJoU",
-
-  //18-01-06
-  "259":"2692q6oTGiA",
-  "260":"IjuaJsOxDio",
-
-  //18-02-10
-  "261":"K56HX_c_MT4",
-  "262":"gUkAxJNB2yU",
-  "263":"6WqtXtnyFqc",
-  "264":"F_X8IXizFAA",
-  "265":"C0fGShB7Zb0",
-  
-  //18-02-25
-  "267":"nA7FUCwGQnc",
-  "268":"qEYprWuuLY8",
-  "269":"YUHe9d3vhnk",
-  
-  //18-03-18
-  "274":"_bKIJAIRcP0",
-  "275":"0MUjB3DUFgE",
-  "276":"-IMIvGW5WkY",
-  "277":"xXflssZe_o4",
-
-  //18-04-22
-  "292":"97b31u8vXKc",
-  "293":"jf4Piy2YLDU",
-  "294":"e52ZPDdaDL0",
-  "295":"ShHLY0dqTBk",
-
-  //18-05-06
-  "300":"hJkVv0W9T-g",
-  "301":"lh4QTxHH5Kk",
-  "302":"neJmXx6ySKA",
-  "303":"rEieAbwXBG8",
-  "304":"YcPWTfaR58k",
-  "305":"CuvGaVJWpNQ",
-
-  //18-05-12
-  "306":"xIQf1cnEujk",
-  "307":"bZ05TFZgUHQ",
-  "310":"IXbf3vuEBBg",
-  "311":"CzqPuozZac4",
-
-  //18-05-27
-  "312":"f5YXugKXTY8",
-  "313":"LB7gHBppBw0",
-  "314":"LJN_qhTy70I",
-  "315":"LLn6AHjtw5g",
-  "316":"akquM5vSRVU",
-  "317":"ddVR9Jhr1xU",
-
-  
-  //18-06-03
-  "318":"rLAmD_dsSRg",
-  "319":"lnIQhfiW2vc",
-  
-  //18-06-17
-  "320":"hWg2Kv73GM8",
-  "321":"Fe2iRsP_-b8",
-  "322":"Op4whNMIqAg",
-  "323":"kIrUgqaexXc",
-  
-  //18-07-08
-  "325":"obPn63EKq8U",
-  "326":"Mqf4Rzr71Lk",
-  "327":"lHhF9ZWFIpg",
-  "328":"aH0NKRp5AqA",
-  "330":"x7XfOr9SwrY",
-
-  //18-08-12
-  "331":"JLfNfohVM58",
-  "332":"mVeGULtR03g",
-  "333":"GncP9F82zUo",
-  "334":"rgeOu3fi7mc",
-
-  //18-09-23
-  "335":"pD_0WCjNUnU",
-  "336":"Qfwh8TnWXqw",
-  "337":"MuL1HaEYu3c",
-  "338":"vuPWnvl_hlY",
-
-  //18-10-14
-  "339":"6hcmIWulPg4",
-  "340":"guZgL7hzpL0",
-  "341":"OTD16Ggxox8",
-
-  //18-10-28
-  "347":"M91apc3KvN4",
-  "348":"4bEjator1pQ",
-  "349":"W0tmIFmrY8I",
-
-  //18-11-18
-  "353":"5nGvDipYFiI",
-  "354":"QUlCGLUqbdc",
-  "355":"FOB0XhnkKF4",
-  //19-03-31
-  "362":"Xpkn4cTpnVc",
-  "363":"5mYBxcgQAyg",
-
-  //19-05-02
-  "371":"h8O8GnYRszM",
-  "372":"R-KL5KXP_K8",
-  "373":"S-0y0VGGUSg",
-  "374":"ctRwmLLzzU4",
-
-  //19-06-08
-  "375":"-BMNHtywKtY",
-  "376":"P9YzUY31v9U",
-
-  //19-07-27
-  "377":"jHvm3RGO_N4",
-  "378":"VUQCvRVD8qs",
-  "379":"GectG8VTesk",
-  "380":"6DGV7WP6QPk",
-  "381":"rD7lz5b8tlI",
-  //19-08-31
-  "382":"2hQJko2xILg",
-  "383":"lQQoib7XviM",
-  //19-09-14
-  "384":"QdP1PHSSW_Q",
-  "385":"CQbcEIdq53w",
-  "386":"5QZe9OjaL-o",
-  "387":"xQHN8d0s40w",
-  "388":"GKNC0LIFTtQ",
-  "443":"7nLn4pbD3gc",
-  //19-09-29
-  "444":"elV8_AwSxeE",
-  "445":"iPN7Z27eVVo",
-  "447":"OmtcSAilUpc",
-  "448":"KwnegOuSuIM",
-
-  //19-10-19
-  450: "fmQndd0p7Z4",
-  451: "9Blvi_lZGMA",
-  452: "-Fdfx_soRyw",
-  
   //佐倉大会
-  405:"KQbhW0XVvnY",
-  414:"F3WWqkGDUrY",
-  416:"K2oSWqVjUcg",
-  420:"AojEV7qeMQc",
-  426:"FXgTZQ5uOLo",
+  "405":"KQbhW0XVvnY",
+  "414":"F3WWqkGDUrY",
+  "416":"K2oSWqVjUcg",
+  "420":"AojEV7qeMQc",
+  "426":"FXgTZQ5uOLo",
 
   //大福大会
   431:"Vl8KZxv2Y68",
   433:"vPWptulX9pc",
   434:"0sPAmdjVAEs",
   442:"Lq0ECSLvrvE",
+
+  //秋季関東リーグ
+  471:"e_UbjBEcylc",
+  472:"D2-AXrXI2CA",
+  482:"92ff2siGorM",
+  483:"92ff2siGorM",
+  484:"OVkmd3tv2WI",
+  485:"OVkmd3tv2WI",
+  503:"OlfMnSLrnHI",
+  504:"OlfMnSLrnHI",
+
+
+  //練習試合
+  531:"5udtjGov7Wc",
+  537:"JVtoM7tlq3w",
+  538:"1rGHnGMOPeQ"
 
 }
