c519c8bd6db183b1a34657be511bb23e3af8339c
ckoshien <ckoshien@gmail.com>
2020-02-14 23:20:20 +0900

    no message

---
+++ config/twitterConfig.js
@@ -0,0 +1,16 @@
+export const team_official_twitter = {
+    57: 'beach_sounds',//横国
+    58: 'CAPHitotsubashi',//一橋
+    61: 'ohmeijicap',//明治
+    63: 'Kns_ctc', //神奈総
+    65: 'TSCappers',//世田谷
+    66: 'cit_capthrow',//千葉工大
+    67: 'gaisei_cap',//蓋世
+    68: 'RikadaiCap',//理科大
+    70: 'HU_cappers',//北大
+    71: 'gaibu_tokai', //蓋鯱->蓋武
+    72: 'kuctc_official',//京大
+    73: 'GCTC2019', //学習院
+    79: 'KUSScapthrowing',//神大附
+    97: 'AomoriCtC'//青森CtC
+}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 
 export { twMovieConfig } from './config/twMovieConfig';
 export { youtubeConfig } from './config/youtubeConfig';
+export { twitterConfig } from './config/twitterConfig';
